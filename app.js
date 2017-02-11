8076a06c210d11f111939d4ba5dc4eb747052b05
Rafa Calin <rafa.calin@yahoo.com>
2017-02-12 00:39:07 +0200

    code indent

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 app.get("/", function(req, res){
-    res.render("home");
+  res.render("home");
 });
 
 app.get("/search", function(req, res){
-    var query = req.query.search;
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=2f959a504378f8e91a4864b4bad6d80e";
-    request(url, function(error, response, body){
-        if(!error && response.statusCode == 200) {
-            var data = JSON.parse(body)
-            res.render("search", {data: data});
-        }
-    });  
+  var query = req.query.search;
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=2f959a504378f8e91a4864b4bad6d80e";
+  request(url, function(error, response, body){
+    if(!error && response.statusCode == 200) {
+      var data = JSON.parse(body)
+      res.render("search", {data: data});
+    }
+  });
 });
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server Start!!!");
+  console.log("Server Start!!!");
 });
